05bb22411e2f0d9661c074d26dfc0e63064c57b8
DolenDeori <dulench.deori564@gmail.com>
2025-01-25 19:19:03 +0530

    IMPROVEMENT: Footer Design.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,30 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="bg-gray-200 text-black md:px-16 lg:px-32 px-4 py-8 flex flex-col gap-8">
+    <footer className="bg-gray-100 text-black md:px-16 lg:px-32 px-4 py-8 flex flex-col gap-8 font-HostGroteskRegular">
       <div className="flex justify-start items-center border-b pb-8 border-gray-400">
         <h1 className="font-HostGroteskMedium text-2xl">Follow Job Search</h1>
         <div>
           <ul className="flex gap-4 ml-10">
             <li>
-              <a href="#">Facebook</a>
+              <a href="#" className="hover:underline">
+                Facebook
+              </a>
             </li>
             <li>
-              <a href="#">Twitter</a>
+              <a href="#" className="hover:underline">
+                Twitter
+              </a>
             </li>
             <li>
-              <a href="#">Instagram</a>
+              <a href="#" className="hover:underline">
+                Instagram
+              </a>
             </li>
             <li>
-              <a href="#">LinkedIn</a>
+              <a href="#" className="hover:underline">
+                LinkedIn
+              </a>
             </li>
           </ul>
         </div>
@@ -25,15 +33,42 @@ export const Footer = () => {
       <div className="flex flex-col md:flex-row">
         <div className=" basis-1/3">
           <h1 className="font-HostGroteskRegular text-2xl">More About Us</h1>
-          <ul className="mt-4 space-y-4 font-HostGroteskRegular opacity-70">
+          <ul className="mt-4 space-y-4 font-HostGroteskRegular text-gray-700">
             <li>
-              <a href="#">About Us</a>
+              <a
+                href="#"
+                className="flex items-center gap-1 hover:text-gray-900"
+                target="_blank"
+              >
+                About Us
+                <span className="material-symbols-outlined text-lg">
+                  open_in_new
+                </span>
+              </a>
             </li>
             <li>
-              <a href="#">Contact Us</a>
+              <a
+                href="#"
+                className="flex items-center gap-1 hover:text-gray-900"
+                target="_blank"
+              >
+                Contact Us{" "}
+                <span className="material-symbols-outlined text-lg">
+                  open_in_new
+                </span>
+              </a>
             </li>
             <li>
-              <a href="#">Our Policy</a>
+              <a
+                href="#"
+                className="flex items-center gap-1 hover:text-gray-900"
+                target="_blank"
+              >
+                Our Policy{" "}
+                <span className="material-symbols-outlined text-lg">
+                  open_in_new
+                </span>
+              </a>
             </li>
           </ul>
         </div>
@@ -41,12 +76,30 @@ export const Footer = () => {
           <h1 className="font-HostGroteskRegular text-2xl">
             Related Information
           </h1>
-          <ul className="mt-4 space-y-4 font-HostGroteskRegular opacity-70">
+          <ul className="mt-4 space-y-4 font-HostGroteskRegular text-gray-600">
             <li>
-              <a href="#">About Us</a>
+              <a
+                href="#"
+                className="flex items-center gap-1 hover:text-gray-900"
+                target="_blank"
+              >
+                Investor Relations{" "}
+                <span class="material-symbols-outlined text-lg">
+                  open_in_new
+                </span>
+              </a>
             </li>
             <li>
-              <a href="#">Contact Us</a>
+              <a
+                href="#"
+                className="flex items-center gap-1 hover:text-gray-900"
+                target="_blank"
+              >
+                Blog{" "}
+                <span class="material-symbols-outlined text-lg">
+                  open_in_new
+                </span>
+              </a>
             </li>
           </ul>
         </div>
@@ -71,23 +124,41 @@ export const Footer = () => {
         </div>
       </div>
       <div className="flex justify-between items-center border-t pt-8 border-gray-400 opacity-70">
-        <div className="flex justify-center items-center gap-4">
+        <div className="flex justify-center items-center gap-16">
           {" "}
           <span className="font-HostGroteskBold text-2xl">Dolen Deori</span>
-          <ul className="flex gap-4 ml-4">
+          <ul className="flex gap-8 ml-4 font-HostGroteskRegular">
             <li>
-              <a href="#">Privacy</a>
+              <a href="#" className="flex items-center gap-1">
+                Privacy{" "}
+                <span class="material-symbols-outlined text-lg">
+                  open_in_new
+                </span>
+              </a>
             </li>
             <li>
-              <a href="#">Applicant & Candidate Privacy</a>
+              <a href="#" className="flex items-center gap-1">
+                Applicant & Candidate Privacy{" "}
+                <span class="material-symbols-outlined text-lg">
+                  open_in_new
+                </span>
+              </a>
             </li>
             <li>
-              <a href="#">Terms</a>
+              <a href="#" className="flex items-center gap-1">
+                Terms{" "}
+                <span class="material-symbols-outlined text-lg">
+                  open_in_new
+                </span>{" "}
+              </a>
             </li>
           </ul>
         </div>
-        <a href="#" className="font-HostGroteskRegular">
+        <a href="#" className="font-HostGroteskRegular flex items-center gap-1">
           Help
+          <span class="material-symbols-outlined text-lg">
+            open_in_new
+          </span>{" "}
         </a>
       </div>
     </footer>
