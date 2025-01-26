4785d0fb9575205190578253e9ee76d8ec31578e
DolenDeori <dulench.deori564@gmail.com>
2025-01-26 23:21:42 +0530

    feat: add jobCard, hero image & improve styling.

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { jobPostings } from "../constant";
 import { CustomButton } from "../components/CustomButton";
+import Card from "../components/JobCard";
+import heroImage from "../assets/images/Hero_Image.jpg";
+import heroImage2 from "../assets/images/Hero_Image2.jpg";
 
 export default function Home() {
   return (
     <main>
-      <section className="bg-gray-100 h-svh flex justify-center items-center">
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="font-HostGroteskBold text-3xl">
-            Search The Opportunity You{" "}
+      <section className="h-svh flex justify-around items-center lg:px-16">
+        <div className="flex flex-col justify-center">
+          <h1 className="font-HostGroteskBold text-5xl">
+            Search The Opportunity <br /> You{" "}
             <span className="text-fuchsia-600">Want</span>{" "}
           </h1>
+          <p className="mt-2 font-HostGroteskRegular text-gray-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Sint
+            in recusandae ipsum, illum dolorem quo!
+          </p>
 
           <form className="mt-5 flex gap-4">
             <input
               type="text"
-              className="p-2 outline-none font-HostGroteskRegular rounded"
+              className="p-2 outline-none font-HostGroteskRegular rounded bg-gray-100 border border-gray-300"
               placeholder="Location"
             />
             <input
               type="text"
-              className="p-2 outline-none font-HostGroteskRegular rounded"
+              className="p-2 outline-none font-HostGroteskRegular rounded bg-gray-100 border border-gray-300"
               placeholder="Job Type"
             />
             <button
@@ -33,23 +40,24 @@ export default function Home() {
             </button>
           </form>
         </div>
+        <div>
+          <img src={heroImage2} alt="" className="h-[500px]" />
+        </div>
       </section>
       <section className="bg-gray-100 lg:px-16 md:px-8 px-4 py-8">
         <h1 className="font-HostGroteskBold text-3xl">Recent Job Postings</h1>
         <div className="grid grid-flow-row mt-10 gap-8 grid-cols-2">
           {jobPostings.map((item) => (
-            <div key={item.id} className="bg-gray-200 p-4 rounded">
-              <img
-                src={`${item.companyLogo}`}
-                alt={`${item.company} company logo`}
-                className="h-[30px] w-[30px]"
-              />
-              <h1 className="font-HostGroteskMedium mt-4 text-lg">
-                {item.jobTitle}
-              </h1>
-              <p className="font-HostGroteskRegular mb-5">{item.description}</p>
-              <CustomButton title={"View Job"} />
-            </div>
+            <Card
+              jobTitle={item.jobTitle}
+              jobDescription={item.description}
+              company={item.company}
+              companyLogo={item.companyLogo}
+              location={item.location}
+              salary={item.salary}
+              skills={item.skillsRequired}
+              level={item.level}
+            />
           ))}
         </div>
         <CustomButton
